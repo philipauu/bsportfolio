799212855e37010c9079d83c52f9aebe980c9f5d
philipauu <Phil@Phils-MacBook.local>
2017-10-11 16:21:05 +0800

    In war, truth is the first casualty. 		-- U Thant

--- js/javascript.js
+++ js/javascript.js
@@ -25,6 +25,38 @@ function send_data() {
   var name = $('#name').val();
   var email = $('#email').val();
   var comments = $('#comments').text();
-  console.log('data: ' + name + ', ' + email + ', ' + comments);
 
+  var contact = {
+    name: name,
+    email: email,
+    comments: comments
+  };
+  console.log(contact);
+  $.get('results.php')
 }
+
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+})
